6f121120775ab9eb6627d0d90d045f0cfec6d955
vansun4 <106801372+vansun4@users.noreply.github.com>
2022-07-20 12:25:39 +1000

    Update App.java

--- src/main/java/au/edu/unsw/infs2605/donationsystem/App.java
+++ src/main/java/au/edu/unsw/infs2605/donationsystem/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Donation Management System");
-        Scene scene = new Scene(loadFXML("app"), 640, 480);
+        Scene scene = new Scene(loadFXML("app"), 1024, 768);
         stage.setScene(scene);
         stage.show();
     }
