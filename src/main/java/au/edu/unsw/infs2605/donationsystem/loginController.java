872afa5b64086bab26686889fd0d41bfd16d598f
vansun4 <106801372+vansun4@users.noreply.github.com>
2022-07-21 02:27:34 +1000

    controllers made for fxml
    assigned fxid for the controllers and tried to add methods to switch screens

---
+++ src/main/java/au/edu/unsw/infs2605/donationsystem/loginController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package au.edu.unsw.infs2605.donationsystem;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author vanessa
+ */
+public class loginController {
+    
+    @FXML 
+    private TextField emailTextField;
+    
+    @FXML 
+    private TextField passwordField;
+    
+    @FXML
+    private Button login;
+
+    @FXML
+    private void switchToHomePage(ActionEvent event) throws IOException {
+        App.setRoot("homepage");
+    }
+    
+}
